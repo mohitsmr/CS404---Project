5298d93d4eb7af64817be3c7009d5f8532f54d8e
ankur_sardar <ankur.sardar18@gmail.com>
2015-11-19 01:19:17 +0530

    door lock alert

---
+++ doorlockalert.js
@@ -0,0 +1,20 @@
+var b = require('bonescript');
+
+setInterval(readTemp,2000);
+
+function readTemp(x) {
+    
+b.analogRead('P9_35', printStatus);
+}
+
+function printStatus(x) {
+    console.log('x.value = ' + x.value);
+    //console.log('x.err = ' + x.err);
+    if(x.value>0.7){
+        console.log('Door is locked.');
+        //setTimeout(readTemp, 250);
+    }
+    else if(x.value<0.7){
+        console.log('Door UNLOCKED!!! INTRUDER!!!')
+    }
+}
