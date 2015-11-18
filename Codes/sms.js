3704df78f0a76ff18e5836ca7dba27d03bcdba0f
ankur_sardar <ankur.sardar18@gmail.com>
2015-11-19 01:55:32 +0530

    sms alert

---
+++ Codes/sms.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var accountSid = 'AC4f4dcf0f1b78d4ef74e2e605b2d0acd1'; 
+var authToken = '[AuthToken]'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+ 
+client.messages.create({ 
+    to: "+919816923644", 
+    from: "+12028310775", 
+    body: "Hey Jenny! Good luck on the bar exam!", 
+      
+}, function(err, message) { 
+    console.log(message.sid); 
+});
