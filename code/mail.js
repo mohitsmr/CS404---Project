ba45ed8c8406940827c993fae043ac53a1fd8e3d
ankur_sardar <ankur.sardar18@gmail.com>
2015-11-19 01:21:38 +0530

    door lock alert

---
+++ code/mail.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'beagleboneiitmandi@gmail.com',
+        pass: 'iitmandi'
+        }
+});
+
+var mailOptions = {
+    from: 'beagleboneiitmandi@gmail.com',
+    to: 'ankur.sardar.as@gmail.com',
+    subject: 'Door Alert',
+    text: 'The Door Is Open',
+    html: '<b>DOOR OPEN</b><p>Please close the door</p>'
+    };
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        }else{
+            console.log('Message Sent: ' + info.response);
+            }
+        });
